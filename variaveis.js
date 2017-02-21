ca5d68af2f95cf8987bda3e54063512f4315cda6
diegopaniago <diegopaniago1@gmail.com>
2017-02-20 20:34:46 -0400

    Criando exemplos de tipos

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+var numero = 1;
+
+var texto = 'Ola mundo';
+
+var lista = [1,2,3,4,5,6];
+
+var object = {};
+
+var funcao = function(){
+	for(var i = 0; i < lista.length; i++){
+		console.log(lista[i]);
+	}
+};
+
+var meDigaSeusTipos = function(){
+	console.log('numero é', typeof numero);
+	console.log('texto é', typeof texto);
+	console.log('lista é', typeof lista);
+	console.log('object é', typeof object);
+	console.log('funcao é', typeof funcao);
+};
