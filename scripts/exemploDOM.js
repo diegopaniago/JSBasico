bf253f1985d439704d4f3441f9355c0695a46bdf
diegopaniago <diegopaniago1@gmail.com>
2017-03-09 21:04:32 -0400

    Criando exemplos de propriedades dda dom

--- scripts/exemploDOM.js
+++ scripts/exemploDOM.js
@@ -1,4 +1,14 @@
+'use strict';
+
 //Navegando as propriedades
+function mostrarOTextoDoTitulo(){
+	var textoDoTitulo = document.getElementById('titulo').innerText;
+	var textoDoTitulo = document.getElementsByClassName('menu')[0].innerText;
+
+	var textoDoTitulo = document.querySelector('header[data-js="menu-da-dom"]').innerText;
+	
+	console.log(textoDoTitulo);
+}
 
 //Seletores
 
--- scripts/variaveis.js
+++ scripts/variaveis.js
@@ -20,6 +20,7 @@ var funcao = function(){
 var meDigaSeusTipos = function(){
 	console.log('numero é', typeof numero);
 	console.log('texto é', typeof texto);
+	//possui representacao Object devido ao fato de se poder fazer uma lista de qualquer tipo
 	console.log('lista é', typeof lista);
 	console.log('object é', typeof object);
 	console.log('funcao é', typeof funcao);
