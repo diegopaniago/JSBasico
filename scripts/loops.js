2808f192bf902667fd4a3ae781609b7ba68a92af
diegopaniago <diegopaniago1@gmail.com>
2017-03-10 13:58:46 -0400

    Limpando codigo

--- scripts/loops.js
+++ scripts/loops.js
@@ -3,25 +3,25 @@
 var listaDeNomes = ['Diego', 'Eduardo', 'Ciclano', 'Fulano'];
 var listaDeCarros = [{carro: 'Uno'}, {carro: 'ferrari'}, {carro: 'gol'}];
 
-var listarNomes = function(){
+function listarNomes(){
 	for(var i=0; i<listaDeNomes.length; i++){
 		console.log(listaDeNomes[i]);
 	}
-};
+}
 
-var listarCarros = function(){
+function listarCarros(){
 	for(var i=0; i<listaDeCarros.length; i++){
 		console.log(listaDeCarros[i].carro);
 	}	
 }
 
-var listarComForEach = function(lista){
+function listarComForEach(lista){
 	lista.forEach(function(elementoDaLista){
 		console.log(elementoDaLista);
 	});
 };
 
-var removerDeUmaListaComWhile = function(lista){
+function removerDeUmaListaComWhile(lista){
 	while(lista.length > 0){
 		console.log(lista);
 		lista.splice(0,1);
--- scripts/variaveis.js
+++ scripts/variaveis.js
@@ -17,18 +17,18 @@ var funcao = function(){
 	}
 };
 
-var meDigaSeusTipos = function(){
+function meDigaSeusTipos(){
 	console.log('numero é', typeof numero);
 	console.log('texto é', typeof texto);
 	//possui representacao Object devido ao fato de se poder fazer uma lista de qualquer tipo
 	console.log('lista é', typeof lista);
 	console.log('object é', typeof object);
 	console.log('funcao é', typeof funcao);
-};
+}
 
-var funcaoComAlert = function(){
+function funcaoComAlert(){
 	for(var i = 0; i < lista.length; i++){
 		console.log(lista[i]);
 		alert('Um alert para a execução do seu códico...');
 	}
-};
+}
